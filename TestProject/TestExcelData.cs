5c125f0c083f20f78572efaf352dce3e9b57a54a
tqk2811 <tqk2811@gmail.com>
2024-09-12 15:05:38 +0700

    update test

--- TestProject/TestExcelData.cs
+++ TestProject/TestExcelData.cs
@@ -14,11 +14,26 @@ namespace TestProject
             [Col("A")]
             public string Id { get; set; } = "";
 
-            [ColRange(ColFlag.SkipReadLineIfCell_NotEmpty, "A", "B", "C")]
+            [ColRange(ColFlag.SkipReadLineIfCell_Empty, "A", "B", "C")]
             public List<string>? Ids { get; set; }
 
-            [ColRange(ColFlag.SkipReadLineIfCell_NotEmpty, "A", "B", "C", "D")]
+            [ColRange(ColFlag.None, "A", "B", "C")]
+            public IEnumerable<string>? Ids2 { get; set; }
+
+            [ColRange(ColFlag.SkipReadLineIfCell_Empty, "A", "B", "C")]
+            public List<string> Ids3 { get; set; } = new();
+
+            [ColRange(ColFlag.SkipReadLineIfCell_Empty, "A", "B", "C")]
+            public IReadOnlyCollection<string>? Ids4 { get; set; }
+
+            [ColRange(ColFlag.SkipReadLineIfCell_Empty, "A", "B", "C", "D")]
             public Dictionary<string, string>? DictIds { get; set; }
+
+            [ColRange('A', 'Z')]
+            public IDictionary<string, string>? DictIds2 { get; set; }
+
+            [ColRange('A', 'Z')]
+            public IReadOnlyDictionary<string, string>? DictIds3 { get; set; }
 
         }
         readonly ExcelServiceReadLine excelServiceReadLine = new ExcelServiceReadLine("ExcelTest.xlsx");
@@ -32,7 +47,19 @@ namespace TestProject
         [Test]
         public async Task Test1()
         {
-            await excelServiceReadLine.GetDatasAsync<TestData>();
+            var datas = await excelServiceReadLine.GetDatasAsync<TestData>();
+            Assert.IsTrue(datas.All(x =>
+            {
+                return
+                    x.Ids is not null && x.Ids.Any() &&
+                    x.Ids2 is not null && x.Ids2.Any() &&
+                    x.Ids3 is not null && x.Ids3.Any() &&
+                    x.Ids4 is not null && x.Ids4.Any() &&
+                    x.DictIds is not null && x.DictIds.Any() &&
+                    x.DictIds2 is not null && x.DictIds2.Any() &&
+                    x.DictIds3 is not null && x.DictIds3.Any()
+                    ;
+            }));
         }
     }
 }
