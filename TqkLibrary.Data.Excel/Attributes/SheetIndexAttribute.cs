0f5429f2415ad9ea24993744a74baf743467cdf4
tqk2811 <tqk2811@gmail.com>
2025-03-11 14:58:04 +0700

    SheetIndexAttribute add StartRow

--- TqkLibrary.Data.Excel/Attributes/SheetIndexAttribute.cs
+++ TqkLibrary.Data.Excel/Attributes/SheetIndexAttribute.cs
@@ -5,24 +5,34 @@ namespace TqkLibrary.Data.Excel.Attributes
 {
     public class SheetIndexAttribute : Attribute
     {
-        public SheetIndexAttribute(int index)
+        public SheetIndexAttribute(int index, int startRow = 1)
         {
             this.Index = index;
+            this.StartRow = startRow;
         }
-        public SheetIndexAttribute(string name)
+        public SheetIndexAttribute(string name, int startRow = 1)
         {
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
             this.Name = name;
+            this.StartRow = startRow;
         }
 
         public int? Index { get; }
         public string? Name { get; }
+        public int StartRow { get; }
 
         public override string ToString()
         {
             return Index.HasValue ? Index.Value.ToString() : Name!;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="worksheets"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public ExcelWorksheet GetSheet(ExcelWorksheets worksheets)
         {
             ExcelWorksheet? excelWorksheet = null;
